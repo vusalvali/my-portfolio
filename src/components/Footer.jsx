d604c30653c2ec05a02c5ecb132ec75d8c45c6f8
Vusal Vali <velivusal@gmail.com>
2024-12-02 16:22:30 +0400

    footer changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,6 @@ import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
 import github from "../assets/img/github.svg";
 
-
 export const Footer = () => {
   return (
     <footer className="footer">
@@ -14,12 +13,16 @@ export const Footer = () => {
         <Row className="align-items-center">
           {/* <MailchimpForm /> */}
           <Col className="text" href="/" size={12} sm={6}>
-            Vusal Valiyev
+            <span className="name-large">Vusal Valiyev</span>
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="https://www.linkedin.com/in/vusal-valiyev/"><img src={navIcon1} alt="Icon" /></a>
-              <a href="https://github.com/vusalvali"><img src={github} alt="" /></a>
+              <a href="https://www.linkedin.com/in/vusal-valiyev/">
+                <img src={navIcon1} alt="Icon" />
+              </a>
+              <a href="https://github.com/vusalvali">
+                <img src={github} alt="" />
+              </a>
               {/* <a href="#"><img src={navIcon3} alt="Icon" /></a> */}
             </div>
             <p>Copyright 2024. All Rights Reserved</p>
@@ -27,5 +30,5 @@ export const Footer = () => {
         </Row>
       </Container>
     </footer>
-  )
-}
+  );
+};
